ce481dcfe8bbe6bee618b9f17b8254ad0c425b4f
Kunal Kataria <kkataria@usc.edu>
2017-10-31 19:36:32 -0700

    upvote and downvote with limiting

--- app/components/reddit-post.js
+++ app/components/reddit-post.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    changeScore(newScore) {
+      // let post = this.get('post');
+      console.log(newScore);
+      this.set('post.data.score', newScore);
+      // this.set(this.get('score'), newScore);
+    }
+  }
 });
--- app/components/vote-control.js
+++ app/components/vote-control.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    this.errors = [];
+    this.set('scorechange', 0);
+  },
+
+  actions: {
+    upvote() {
+      let scorechange = this.get('scorechange');
+      if (scorechange < 1) {
+        let newVoteValue = this.get('score') + 1;
+        this.set('scorechange', this.get('scorechange') + 1);
+        this.get('onvote')(newVoteValue);
+      }
+    },
+
+    downvote() {
+      let scorechange = this.get('scorechange');
+      if (scorechange > -1) {
+        let newVoteValue = this.get('score') - 1;
+        this.set('scorechange', this.get('scorechange') - 1);
+        this.get('onvote')(newVoteValue);
+      }
+    }
+  }
 });
